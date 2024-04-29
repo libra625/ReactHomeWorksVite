ebddcd1fecc1d0ad88bc6022b570b3d8f55275f6
libra625 <18.12.17.moyo@gmail.com>
2024-04-29 19:45:42 +0300

    initial commit 4

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,23 +1,23 @@
-import React from 'react';
+import React from "react";
 
 class Card extends React.Component {
-    render() {
-        const {title, text} = this.props;
+  render() {
+    const { title, text } = this.props;
 
-        return (
-            <div className="card">
-                <div className="card-body">
-                    {title ? <h4 className="card-title">{title}</h4> : null}
-                    {text ? <p className="card-title">{text}</p>: null}
-                </div>
-            </div>
-        )
-    }
+    return (
+      <div className="card">
+        <div className="card-body">
+          {title ? <h4 className="card-title">{title}</h4> : null}
+          {text ? <p className="card-title">{text}</p> : null}
+        </div>
+      </div>
+    );
+  }
 }
 
 Card.defaultProps = {
-    title: null,
-    text: null
-}
+  title: null,
+  text: null,
+};
 
 export default Card;
