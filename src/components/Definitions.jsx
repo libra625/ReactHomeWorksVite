2a40a43e1d5ad3ee5888597ca2916b63e0e48159
libra625 <18.12.17.moyo@gmail.com>
2024-04-30 16:18:42 +0300

    updated Definitions.jsx

--- src/components/Definitions.jsx
+++ src/components/Definitions.jsx
@@ -1,28 +1,26 @@
-import React from 'react';
+import React from "react";
 
 class Definitions extends React.Component {
-    render() {
-        const { data } = this.props;
-        console.log(data);
-        return (
-            <dl>
-                {
-                    data.map(definition => (
-                        <React.Fragment key={definition.id}>
-                            <dt>{definition.dt}</dt>
-                            <dd>{definition.dd}</dd>
-                            <br/>
-                        </React.Fragment>
-                    ))
-                }
-            </dl>
-        )
-    }
+  render() {
+    const { data } = this.props;
+    console.log(data);
+    return (
+      <dl>
+        {data.map((definition) => (
+          <React.Fragment key={definition.id}>
+            <dt>{definition.dt}</dt>
+            <dd>{definition.dd}</dd>
+            <br />
+          </React.Fragment>
+        ))}
+      </dl>
+    );
+  }
 }
 
 Definitions.defaultProps = [
-    { dt: 'one', dd: 'two', id: 1 },
-    { dt: 'another term', dd: 'another description', id: 2 },
-]
+  { dt: "one", dd: "two", id: 1 },
+  { dt: "another term", dd: "another description", id: 2 },
+];
 
 export default Definitions;
