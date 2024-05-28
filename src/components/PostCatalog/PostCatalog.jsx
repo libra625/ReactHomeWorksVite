e64615787319bf0c7fada54380fba1e8c1e154a7
libra625 <18.12.17.moyo@gmail.com>
2024-05-28 16:35:04 +0300

    done

--- src/components/PostCatalog/PostCatalog.jsx
+++ src/components/PostCatalog/PostCatalog.jsx
@@ -1,49 +1,47 @@
-import { Component } from "react";
+import { useEffect, useState } from "react";
 import { ListGroup } from "react-bootstrap";
 import Post from "../Post";
 
-class PostCatalog extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            posts: [],
-        };
-    }
+const PostCatalog = () => {
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //         posts: [],
+    //     };
+    // }
+    const [posts, setPosts] = useState([]);
 
-    async componentDidMount() {
-        await this.getPosts();
-    }
-
-    getPosts = async () => {
-        try {
-            const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-            const posts = await res.json();
-            this.setState({ posts });
-        } catch (error) {
-            console.dir(error);
-            throw Error(error);
-        }
+    const getPosts = async () => {
+    // try {
+        const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+        const posts = await res.json();
+        setPosts(posts);
+    // } catch (error) {
+    //     console.dir(error);
+    //     throw Error(error);
+    // }
     };
 
-    render() {
-        const { posts } = this.state;
-        return (
-            <div className="posts">
-                <ListGroup className="posts__list">
-                    {posts.map((post) => {
-                        return (
-                            <Post
-                                title={post.title}
-                                id={post.id}
-                                content={post.body}
-                                key={post.id}
-                            ></Post>
-                        );
-                    })}
-                </ListGroup>
-            </div>
-        );
-    }
-}
+    useEffect(() => {
+        getPosts().catch(console.error);
+    }, []);
+
+    return (
+        <div className="posts">
+            <ListGroup className="posts__list">
+                {posts.map((post) => {
+                    return (
+                        <Post
+                            title={post.title}
+                            id={post.id}
+                            content={post.body}
+                            key={post.id}
+                        ></Post>
+                    );
+                })}
+            </ListGroup>
+        </div>
+    );
+};
 
 export default PostCatalog;
