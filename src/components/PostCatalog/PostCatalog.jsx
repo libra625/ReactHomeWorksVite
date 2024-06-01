13338442fd5b420870c9a23d50360b92964e7a05
libra625 <18.12.17.moyo@gmail.com>
2024-06-01 20:09:11 +0300

    done

--- src/components/PostCatalog/PostCatalog.jsx
+++ src/components/PostCatalog/PostCatalog.jsx
@@ -3,27 +3,21 @@ import { ListGroup } from "react-bootstrap";
 import Post from "../Post";
 
 const PostCatalog = () => {
-    // constructor(props) {
-    //     super(props);
-    //     this.state = {
-    //         posts: [],
-    //     };
-    // }
     const [posts, setPosts] = useState([]);
 
     const getPosts = async () => {
-    // try {
-        const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-        const posts = await res.json();
-        setPosts(posts);
-    // } catch (error) {
-    //     console.dir(error);
-    //     throw Error(error);
-    // }
+        try {
+            const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+            const posts = await res.json();
+            setPosts(posts);
+        } catch (error) {
+            console.dir(error);
+            throw Error(error);
+        }
     };
 
     useEffect(() => {
-        getPosts().catch(console.error);
+        getPosts();
     }, []);
 
     return (
